b5794d49cdbe3c96b2dfb6e0b9a13bf0e9767136
Juan Jose <chambijuanjose05@gmail.com>
2023-11-07 20:39:28 -0300

    Feat/HeaderLandingPage

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -3,17 +3,15 @@ import logo from "../../assets/logoJCDVPNG.png"
 
 function LandingPage() {
   return (
-    <header className="flex h-screen w-screen justify-center items-start ">
+    <header className="flex flex-col h-screen w-screen justify-start items-center ">
       
       <nav className="area h-20 flex justify-between items-center text-gray-800 ">
 
           <picture className="flex justify-center items-center max-w-[100px] min-h-[80%">
-            <img src={logo} alt="Jesus Camino de Vida" className="w-[70%]" />
+            <img src={logo} alt="Jesus Camino de Vida" className="w-[65%]" />
           </picture>
-          
-        {/* <div className="">   Para un menu Responsive  */}
 
-          <ul className="hidden justify-around items-center font-bold w-[40%] sm:flex">
+          <ul className="hidden justify-around items-center font-bold w-[35%] sm:flex ">
             <li className="hover:text-blue-light transitionColor">
               <a href="#">Ministerios</a>
             </li>
@@ -25,18 +23,20 @@ function LandingPage() {
             </li>
           </ul>
 
-        {/* </div> */}
-
-        <ul className="hidden min-w-[19%]  justify-between items-center max-phone:flex lg:flex">
-          <li>
-            <a href="#" className="button">Log In</a>
-          </li>
-          <li>
-            <a href="#" className="button">Sign In</a>
-          </li>
-        </ul>
-
       </nav>
+
+      <section className="area flex flex-row justify-between py-28">
+
+        <article className="w-[40%]">
+          <h1 className="text-6xl font-black py-5">Congregate</h1>
+          <p className="font-regular">Bienvenidos a nuestra comunidad de fe, un lugar donde la gracia y el amor de Cristo se manifiestan en cada rincón. En nuestra iglesia evangélica, creemos que la fe en Jesucristo es la fuente de toda esperanza y transformación. Aquí, te invitamos a un viaje de descubrimiento espiritual, a encontrar un propósito más profundo en la vida y a construir relaciones significativas con Dios y con los demás.</p>
+        </article>
+
+        <picture className="flex justify-center w-[45%] ">
+          <img src={logo} alt="" />
+        </picture>
+
+      </section>
     
     </header>
   )
