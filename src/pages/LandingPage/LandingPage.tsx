f4748ea57311069411b8456baab18bcc73bf12de
Juan Jose <chambijuanjose05@gmail.com>
2023-11-08 20:13:25 -0300

    Fix/TextBug

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -37,13 +37,13 @@ function LandingPage() {
 
         </nav>
 
-        <section className="area flex flex-row justify-between py-10 sm:py-16 flex-wrap">
+        <section className="area flex flex-row justify-between py-10 sm:py-16 flex-wrap bg-lime-500">
 
           <article className="w-[560px] flex-grow">
-            <h1 className="text-6xl font-black" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <h1 className="flex justify-center items-center text-5xl  font-black min-h-[200px] " onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 {hovered ? "Bienvenido a Jesús Camino de Vida: Donde la Fe Encuentra un Hogar." : "Encuentra Tu Camino en Jesús: Únete a Nuestra Comunidad." }
             </h1>
-            <p className="font-regular py-6">Bienvenidos a nuestra comunidad de fe, un lugar donde la gracia y el amor de Cristo se manifiestan en cada rincón. En nuestra iglesia evangélica, creemos que la fe en Jesucristo es la fuente de toda esperanza y transformación. Aquí, te invitamos a un viaje de descubrimiento espiritual, a encontrar un propósito más profundo en la vida y a construir relaciones significativas con Dios y con los demás.</p>
+            <p className="font-regular ">Bienvenidos a nuestra comunidad de fe, un lugar donde la gracia y el amor de Cristo se manifiestan en cada rincón. En nuestra iglesia evangélica, creemos que la fe en Jesucristo es la fuente de toda esperanza y transformación. Aquí, te invitamos a un viaje de descubrimiento espiritual, a encontrar un propósito más profundo en la vida y a construir relaciones significativas con Dios y con los demás.</p>
           </article>
 
           <picture className="flex justify-center items-center flex-grow w-[500px]">
@@ -54,7 +54,14 @@ function LandingPage() {
 
       </header>
       <main>
-
+        <picture>
+          <img src="" alt="" />
+        </picture>
+        <section>
+          <article>
+          
+          </article>
+        </section>
       </main>
     </>
   )
